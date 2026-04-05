d9250210faace7fc0ce145c8c587768a8efe49a6
esperithm16 <rajatshaily17@gmail.com>
2026-04-05 12:19:57 +0530

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+    int n=s1.length();
+    char[] s1arr=s1.toCharArray();
+    Arrays.sort(s1arr);
+    for(int i =0 ;i<=s2.length()-n;i++){
+        char[] window=s2.substring(i,i+n).toCharArray();
+        Arrays.sort(window);
+        if(Arrays.equals(s1arr,window)){return true;}
+    }    return false;
+    }
+}
